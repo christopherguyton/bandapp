35cb39eb7dea5bb87d0ff46fd19d0b39098db62b
MrBehavior <cguyton87@gmail.com>
2020-08-05 15:59:40 -0700

    Add Song To Set Buttons Functioning

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+
+//Adding Elements to fields
+
+function addSet1Song(){
+var setInput = document.getElementById('song').value
+const newSong = document.createElement('li');
+newSong.className = 'collection-item';
+newSong.appendChild(document.createTextNode(setInput));
+document.getElementById('set-1').appendChild(newSong);
+document.getElementById('song').value = '';
+}
+
+function addSet2Song(){
+var setInput = document.getElementById('song').value
+const newSong2 = document.createElement('li');
+newSong2.className = 'collection-item';
+newSong2.appendChild(document.createTextNode(setInput));
+document.getElementById('set-2').appendChild(newSong2);
+document.getElementById('song').value = '';
+}
+
+
+console.log(newSong)
+
+//Variables
 const firstList = document.getElementById('set-1')
 const secondList = document.getElementById('set-2')
 
-
-
-function songSelected() {
-
-}
 
